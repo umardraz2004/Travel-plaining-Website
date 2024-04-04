09aeb8c80f17d64ea5bc38286034ef65832c3436
umardraz1115 <umardaraz2925@gmail.com>
2024-04-05 00:16:34 +0500

    Changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,17 @@
+// Navbar pop up
 const toggleBtn = document.querySelector('#icon');
 const dropDownMenu = document.querySelector('.pop-up');
-toggleBtn.onclick = function () {
+toggleBtn.addEventListener('click' , function (e) {
     dropDownMenu.classList.toggle('open');
-}
+});
+
+const selectBtn = document.querySelector('.selectBtn');
+const hiddenElement = document.querySelector('.hidden');
+const bookedBtn = document.getElementById('booked');
+selectBtn.addEventListener('click' , function(e) {
+    hiddenElement.style.display = 'block';
+});
+bookedBtn.addEventListener('click', function(e) {
+    hiddenElement.style.display = 'none';
+});
+
